9635e0f474c4113eeccf3c712e1d44a4138301ff
Alison <odevalison@gmail.com>
2025-08-06 03:11:13 -0300

    feat: add sign out

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignInForm from "./components/sign-in-form";
@@ -5,21 +6,25 @@ import SignUpForm from "./components/sign-up-form";
 
 export default function Authentication() {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
-        </TabsList>
+    <>
+      <Header />
 
-        <TabsContent value="sign-in">
-          <SignInForm />
-        </TabsContent>
+      <div className="flex w-full flex-col gap-6 p-5">
+        <Tabs defaultValue="sign-in">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="sign-up">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+          <TabsContent value="sign-in">
+            <SignInForm />
+          </TabsContent>
+
+          <TabsContent value="sign-up">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 }
