ce646e388791e0a083ca29a670370ccf6344f0cc
Alison <odevalison@gmail.com>
2025-08-10 12:42:23 -0300

    feat: add checkout success page

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -2,25 +2,13 @@
 
 import { loadStripe } from "@stripe/stripe-js";
 import { Loader2 } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
 
 import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
-import { CardHeader } from "@/components/ui/card";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 
 const FinishOrderButton = () => {
   const finishOrderMutation = useFinishOrder();
-  const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
 
   const handleFinishOrder = async () => {
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
@@ -38,8 +26,6 @@ const FinishOrderButton = () => {
     }
 
     await stripe.redirectToCheckout({ sessionId: checkoutSession.id });
-
-    setSuccessDialogIsOpen(true);
   };
 
   return (
@@ -53,41 +39,6 @@ const FinishOrderButton = () => {
         {finishOrderMutation.isPending && <Loader2 className="animate-spin" />}
         Finalizar a compra
       </Button>
-
-      <Dialog open={successDialogIsOpen} onOpenChange={setSuccessDialogIsOpen}>
-        <DialogContent className="text-center">
-          <CardHeader>
-            <Image
-              src="/dialog-image.svg"
-              alt="Pedido efetuado com sucesso!"
-              width={250}
-              height={230}
-              className="mx-auto"
-            />
-          </CardHeader>
-
-          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
-          <DialogDescription>
-            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
-            na seção de “Meus Pedidos”.
-          </DialogDescription>
-
-          <DialogFooter>
-            <Button size="lg" className="rounded-full">
-              Ver meus pedido
-            </Button>
-
-            <Button
-              size="lg"
-              className="rounded-full"
-              variant="outline"
-              asChild
-            >
-              <Link href="/">Página inicial</Link>
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </>
   );
 };
---
+++ src/app/checkout/success/page.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Header from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+import { CardHeader } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const CheckoutSuccessPage = () => {
+  return (
+    <>
+      <Header />
+
+      <Dialog open={true}>
+        <DialogContent className="text-center">
+          <CardHeader>
+            <Image
+              src="/dialog-image.svg"
+              alt="Pedido efetuado com sucesso!"
+              width={250}
+              height={230}
+              className="mx-auto"
+            />
+          </CardHeader>
+
+          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
+          <DialogDescription>
+            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
+            na seção de “Meus Pedidos”.
+          </DialogDescription>
+
+          <DialogFooter>
+            <Button size="lg" className="rounded-full">
+              <Link href="/my-orders">Ver meus pedido</Link>
+            </Button>
+
+            <Button
+              size="lg"
+              className="rounded-full"
+              variant="outline"
+              asChild
+            >
+              <Link href="/">Página inicial</Link>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default CheckoutSuccessPage;
